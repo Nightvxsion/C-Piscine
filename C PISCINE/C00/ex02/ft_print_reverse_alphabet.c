15a03116749cb8e2d48825eb34a656bb60295691
Nightvxsion <nightvisionmorespace@gmail.com>
2024-09-08 01:18:27 +0200

    Update ft_print_reverse_alphabet.c

--- C PISCINE/C00/ex02/ft_print_reverse_alphabet.c
+++ C PISCINE/C00/ex02/ft_print_reverse_alphabet.c
@@ -29,3 +29,9 @@ void	ft_print_reverse_alphabet(void)
 	ft_print_reverse_alphabet();
 	return (0);
 }*/
+// ES:
+// 1. Es lo mismo que el ejercicio anterior solo que en el bucle tipo while, decimos que z (con la letra z) sea MAYOR o igual que el caracter a
+// 2. En vez de que imprima el caracter de delante, decimos que imprima hacia atrás (z--).
+// EN:
+// 1. It's the same exercise as the previous but here in the loop, we say that the variable z (with the letter z) it's oging to be GREATER or equal than the letter a
+// 2. Inside the loop, instead of print the next letter, we're going to print backwards (z--).
