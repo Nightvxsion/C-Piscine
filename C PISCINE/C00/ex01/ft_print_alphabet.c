2aa144b3d7f015884cd082778de8e3017df42381
Nightvxsion <nightvisionmorespace@gmail.com>
2024-09-08 01:14:36 +0200

    ft_print_alphabet.c

--- C PISCINE/C00/ex01/ft_print_alphabet.c
+++ C PISCINE/C00/ex01/ft_print_alphabet.c
@@ -29,3 +29,11 @@ void	ft_print_alphabet(void)
 	ft_print_alphabet();
 	return (0);
 }*/
+// ES: 
+// 1. Tenemos una funcion llamada "ft_print_alphabet" y creamos una variable "char" con el nombre que tu quieras (a), cuya variable inicializada desde 'a'
+// 2. Creamos un bucle "while" que haga la condición que 'a' sea menor o igual que 'z' (que se va a cumplir siempre)
+// 3. Y con el comando "write" mandamos que imprima la variable a y que imprima también el caracter que tiene al lado (es decir, a, b ,c ...)
+// EN:
+// 1. We've a function called "ft_print_alphabet" and we create a variable type "char" called however you want (in this case "a"), which variable inicialized on letter 'a'
+// 2. We create a loop type "while" that does the condition that says 'a' is less or equal than 'z' (is gonna be done always)
+// 3. With the "write" command we're going to print the character that 'a' contains (a), and the character next to that (for example: a, b, c ...)
