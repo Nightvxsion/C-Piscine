5378965de905e17132c04ec1efbce641d749e28d
Nightvxsion <nightvisionmorespace@gmail.com>
2024-09-10 16:31:27 +0200

    ft_is_negative.c

--- C PISCINE/C00/ex04/ft_is_negative.c
+++ C PISCINE/C00/ex04/ft_is_negative.c
@@ -34,3 +34,11 @@ void	ft_is_negative(int n)
 	ft_is_negative(-1);
 	return (0);
 }*/
+// ES:
+// 1. Como siempre, hacemos la función ft_putchar para detectar los caracteres
+// 2. Creamos una función llamada ft_is_negative que reciba un "int" en el que establezcamos una condición que si n es MAYOR o igual que 0
+// entonces con ft_putchar imprimir "P" (Positivo) si es otro número que NO sea positivo, que imprima "N" (Negativo)
+// EN:
+// 1. As always, we make the function ft_putchar to detect characters
+// 2. We create a function called ft_is_negative that recieves an integer to stablish a condition that if n is GREATER or equal to 0
+// then print the letter "P" (Positive) with the function ft_putchar, if it's a NON-positive number, print the letter "N" (Negative) 
